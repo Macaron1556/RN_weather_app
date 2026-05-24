aaaa62be267f61cb5174faa83137fee94e87b991
Macaron1556 <kittysi1027@gmail.com>
2026-05-25 00:16:26 +0900

    컴포넌트들 다뤄보기

--- App.js
+++ App.js
@@ -10,9 +10,9 @@ export default function App() {
     <View style={styles.container}>
       <Text style={{...styles.text, color: "blue"}}>리액트 네이티브</Text>
       <Text style={{fontSize: 100}}>결과 : {number}</Text>
-      <View style={{ flexDirection: "row", gap : 10}}>
-        <Button title="증가" onPress={() => setNumber(number + 1)}/>
-        <Button title="감소" onPress={() => setNumber(number - 1)}/>
+      <View style={styles.btnGroup}>
+        <Button style = {styles.btn} title="증가" onPress={() => setNumber(number + 1)}/>
+        <Button style = {styles.btn} title="감소" onPress={() => setNumber(number - 1)}/>
       </View>
       <StatusBar backgroundColor="red" barStyle="light-content" hidden={false}/>
     </View>
@@ -30,5 +30,12 @@ const styles = StyleSheet.create({
     fontSize : 50,
     color : "red",
     fontWeight : "bold",
+  },
+  btnGroup: {
+    flexDirection: "row",
+    gap : 10,
+  },
+  btn: {
+    width: 100,
   },
 });
